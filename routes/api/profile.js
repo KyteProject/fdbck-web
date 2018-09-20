0208da7739bec8184387add9f0bcbc5302992c00
Dan <headhertz88@gmail.com>
2018-09-20 21:52:00 +0200

    profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,6 +27,57 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 			.json(err));
 });
 
+// @route		GET /api/profile/:name
+// @desc		Get user profile by name
+// @access	Public
+router.get('/user/:name', (req, res) => {
+	const errors = {};
+
+	User.findOne({ name: req.params.name })
+		.then(user => {
+			if (!user) {
+				errors.noprofile = 'There is no profile for this user';
+				return res.status(404)
+					.json(errors);
+			}
+
+			Profile.findOne({ user: user._id })
+				.populate('user', ['name', 'avatar'])
+				.then(profile => {
+					if (!profile) {
+						errors.noprofile = 'There is no profile for this user';
+						return res.status(404)
+							.json(errors);
+					}
+
+					res.json(profile);
+				})
+				.catch(err => res.json);
+		})
+		.catch(err => console.log(err));
+});
+
+// @route		GET /api/profile/id/:user_id
+// @desc		Get user profile by ID
+// @access	Public
+router.get('/id/:user_id', (req, res) => {
+	const errors = {};
+
+	Profile.findOne({ user: req.params.user_id })
+		.populate('user', ['name', 'avatar'])
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = 'There is no profile for this user';
+				return res.status(404)
+					.json(errors);
+			}
+
+			res.json(profile);
+		})
+		.catch(err => res.status(404)
+			.json(err));
+});
+
 // @route		POST /api/profile
 // @desc		Create or edit user profile
 // @access	Private
@@ -65,6 +116,27 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 					.then(profile => res.json(profile));
 			}
 		});
+});
+
+// @route		POST /api/profile/all
+// @desc		View all profiles
+// @access	Public
+router.get('/all/', (req, res) => {
+	const errors = {};
+
+	Profile.find()
+		.populate('user', ['name', 'avatar'])
+		.then(profiles => {
+			if (!profiles) {
+				errors.noprofile = 'Unable to find any profiles';
+				return res.status(404)
+					.json(errors);
+			}
+
+			res.json(profiles);
+		})
+		.catch(err => res.status(404)
+			.json(err));
 });
 
 module.exports = router;
