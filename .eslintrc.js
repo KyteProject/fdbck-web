ab5b184089ed06ba570d772c5eacd9186787658d
Dan <headhertz88@gmail.com>
2018-10-24 09:47:39 +0200

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,64 +1,75 @@
 module.exports = {
-	"extends": "eslint:recommended",
-	"parserOptions": {
-		"ecmaVersion": 7,
-		"sourceType": "script",
-		"ecmaFeatures": {
-			"jsx": true
-		}
-	},
-	"rules": {
-		"global-require": "warn",
-		"import/no-dynamic-require": "off",
-		"no-console": "off",
-		"no-param-reassign": "off",
-		"no-undef": "off",
-		"indent": [
-      "error",
-      "tab",
-			{
-				"SwitchCase": 1
-      }
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  rules: {
+    'react/prop-types': 0,
+  },
+  plugins: ['react', 'import', 'jsx-a11y'],
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      version: '16.5.2',
+      flowVersion: '0.53',
+    },
+    propWrapperFunctions: ['forbidExtraProps'],
+  },
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  rules: {
+    // "global-require": "warn",
+    // "import/no-dynamic-require": "off",
+    // "no-console": "off",
+    // "no-param-reassign": "off",
+    // "no-undef": "off",
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+    'keyword-spacing': [
+      'error',
+      {
+        before: true,
+        after: true,
+      },
     ],
-		"linebreak-style": [
-      "error",
-      "unix"
+    'space-before-blocks': [
+      'error',
+      {
+        functions: 'always',
+        keywords: 'always',
+        classes: 'always',
+      },
     ],
-		"quotes": [
-      "warn",
-      "single"
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always',
+      },
     ],
-		"semi": [
-      "warn",
-      "always"
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+        ignoreReadBeforeAssign: false,
+      },
     ],
-		"keyword-spacing": [
-      "error", {
-				"before": true,
-				"after": true
-      }
-    ],
-		"space-before-blocks": [
-      "error", {
-				"functions": "always",
-				"keywords": "always",
-				"classes": "always"
-      }
-    ],
-		"space-before-function-paren": [
-      "error", {
-				"anonymous": "never",
-				"named": "never",
-				"asyncArrow": "always"
-      }
-    ],
-		"prefer-const": [
-      "error", {
-				"destructuring": "all",
-				"ignoreReadBeforeAssign": false
-      }
-    ],
-	}
+  },
 };
-
-// "extends": "eslint:recommended",
--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -4,28 +4,28 @@ import Login from '../auth/Login';
 import Register from '../auth/Register';
 
 const sectionStyle = {
-	backgroundImage: `url(../../assets/img/backgrounds/headphones-large.jpg)`,
-	backgroundRepeat: 'no-repeat',
-	height: '100vh',
-	width: '100vw',
-	backgroundPosition: 'left center',
+  backgroundImage: 'url(../../assets/img/backgrounds/headphones-large.jpg)',
+  backgroundRepeat: 'no-repeat',
+  height: '100vh',
+  width: '100vw',
+  backgroundPosition: 'left center',
 };
 
 export default class Landing extends Component {
-	render() {
-		return (
-			<div className="page">
-				<div className="page__content" id="page-content">
-					<div
-						className="content d-none d-lg-block"
-						id="content"
-						style={sectionStyle}
-					/>
-					<Route exact path="/" component={Login} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/register" component={Register} />
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="page">
+        <div className="page__content" id="page-content">
+          <div
+            className="content d-none d-lg-block"
+            id="content"
+            style={sectionStyle}
+          />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+      </div>
+    );
+  }
 }
